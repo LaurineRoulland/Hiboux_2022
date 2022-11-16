6d4422803675fd506ec494d1e0c13db0521cb290
LaurineRoulland <l.roulland@hotmail.fr>
2022-11-16 10:08:54 +0100

    Compteur JS
    Changement de compteur Javascript.
    "mariage dans... x date" -> "mariage passé depuis... x date"

--- index.php
+++ index.php
@@ -33,7 +33,8 @@ require_once('includes/header.php');
         <main class="home">
             
             <aside>
-                <div id="date">
+                <!-- DECOMPTE AVANT LE JOUR J -->
+                <!-- <div id="date">
                     <h2>Le 10 Septembre</h2>
                     <div id="decompte_rebour">
                         <p>Mariage dans... <br>
@@ -57,8 +58,37 @@ require_once('includes/header.php');
                             </script>
                         </p>
                     </div>
+                </div> -->
+
+                <!--  COMPTEUR DEPUIS LE JOUR DU MARIAGE -->
+
+                <div id="date">
+                    <h2>Le 10 Septembre</h2>
+                    <div id="decompte_rebour">
+                        <p>Mariage passé depuis <br>
+                            <label id="decompte"></label>
+                            <script type="text/javascript">
+                                var Affiche=document.getElementById("decompte");
+                                function Rebour() {
+                                    var date1 = new Date("Sep 10, 2022 14:00:00");
+                                    var date2 = new Date();
+                                    var sec = (date2 - date1) / 1000;
+                                    var n = 24 * 3600;
+                                    if (sec > 0) {
+                                        j = Math.floor (sec / n);
+                                        h = Math.floor ((sec - (j * n)) / 3600);
+                                        mn = Math.floor ((sec - ((j * n + h * 3600))) / 60);
+                                        sec = Math.floor (sec - ((j * n + h * 3600 + mn * 60)));
+                                        Affiche.innerHTML = j +" jours <br>"+ h +" h "+ mn +" min et "+ sec + " sec";
+                                        window.status = "Temps restant : " + j +" j "+ h +" h "+ mn +"min "+ sec + "s ";
+                                    }
+                                tRebour=setTimeout ("Rebour();", 1000);
+                                }
+                                Rebour();
+                            </script>
+                        </p>
+                    </div>
                 </div>
-                
             </aside>
 
             <div class="logo_programme">
